d03947abe3da5c8c43aaf85892ec8b863f4824d2
iapposki <konark981999@gmail.com>
2023-07-25 19:21:44 +0530

    adding routes

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 import express, {Request, Response, Express} from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app : Express = express();
 const port : Number = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(cors());
+
 // setup view engine
 app.set('view engine', 'ejs');
 
-
+// create home route
+app.get('/', (req : Request, res : Response) => {
+    res.render('home')
+})
 
 
 if (require.main === module) {
---
+++ routes/auth.routes.ts
@@ -0,0 +1,8 @@
+import express, {Router, Request, Response} from "express";
+
+const router : Router = express.Router();
+
+// auth login
+router.get('/login', () => {
+
+})
